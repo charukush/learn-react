90deed2687994f9c05fe1c2f6e5ef3e12ff6a520
Charu Kush <charu.kush@gmail.com>
2023-04-19 17:41:44 -0700

    TypeScript fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hello World!!
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       Hello World!!
+//     </div>
+//   );
+// }
+type CarParams = {
+  color: string
+}
+function Car(props:CarParams){
+  return(
+    <h2>This is a {props.color} Car function.</h2>
+  )
 }
 
-export default App;
+export default Car;
